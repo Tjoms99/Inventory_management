c6123b4ba8db498fba71294afd8c38d56d0284c7
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-06-19 18:18:53 +0200

    Updated server files

--- flutter_db/addAccount.php
+++ flutter_db/addAccount.php
@@ -7,8 +7,10 @@ $account_role = $_POST['account_role'];
 $password = $_POST['password'];
 $rfid = $_POST['rfid'];
 $customer_id = $_POST['customer_id'];
+$registered_customer_id = $_POST['registered_customer_id'];
 
 
-$sql = "INSERT INTO `accounts`(`account_name`, `account_role`, `password`, `RFID`, `customer_id`, `registered_customer_id`) VALUES ('$account_name','$account_role','$password','$rfid','$customer_id',0)";
+
+$sql = "INSERT INTO `accounts`(`account_name`, `account_role`, `password`, `rfid`, `customer_id`, `registered_customer_id`) VALUES ('$account_name','$account_role','$password','$rfid','$customer_id', '$registered_customer_id')";
 
 $conn->query($sql);
