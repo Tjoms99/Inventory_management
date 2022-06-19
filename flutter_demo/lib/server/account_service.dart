9749cca6bd5c02beafc676aee03744182c305648
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-06-19 21:26:17 +0200

    Login matches with only one user + added hash for password

--- flutter_demo/lib/server/account_service.dart
+++ flutter_demo/lib/server/account_service.dart
@@ -21,9 +21,14 @@ Future<List<Account>> getAccounts() async {
   return accounts;
 }
 
-Future<Account> getAccount() async {
-  var uri = Uri.parse("http://192.168.1.201/dashboard/flutter_db/getAccount.php");
-  final response = await http.get(uri);
+Future<Account> getAccount(Account thisAccount) async {
+  var uri =
+      Uri.parse("http://192.168.1.201/dashboard/flutter_db/getAccount.php");
+  final response = await http.post(uri, body: {
+    "account_name": thisAccount.accountName,
+    "password": thisAccount.password,
+  });
+
   Account account = createAccountFromJson(jsonDecode(response.body) as List, 0);
   return account;
 }
