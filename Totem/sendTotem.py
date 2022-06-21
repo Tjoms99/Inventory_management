895076fcabae6c69b7fd37eefd1e2bbf75b671de
Hoelland <s299610@studenti.polito.it>
2022-06-21 23:30:24 +0200

    sendTotem.py comments

--- Totem/sendTotem.py
+++ Totem/sendTotem.py
@@ -1,15 +1,17 @@
-import requests
-from getmac import get_mac_address
-
 import time
-from mfrc522 import SimpleMFRC522
-import RPi.GPIO as GPIO
+
+import requests							# http request library
+from getmac import get_mac_address		# library for getting device mac address
+
+from mfrc522 import SimpleMFRC522		# RFID module library
+import RPi.GPIO as GPIO					# Raspberry pi GPIO library
 
 api_endpoint = "http://192.168.1.201/dashboard/flutter_db/addTotem.php"
 mac = get_mac_address()
 
 reader = SimpleMFRC522()
 
+# Function waits for an RFID tag and makes a request to the web-server, and repeats. 
 def main():
 	while True:
 		rfid, text = reader.read()
