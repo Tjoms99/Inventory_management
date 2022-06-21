2db87b5c698bf39d9c74ee3c9692cbf8f1e40d63
Hoelland <s299610@studenti.polito.it>
2022-06-21 19:35:50 +0200

    fixed addTotem.php

--- flutter_db/addTotem.php
+++ flutter_db/addTotem.php
@@ -4,6 +4,6 @@ include 'conn.php';
 $mac = $_POST['mac'];
 $rfid = $_POST['rfid'];
 
-$sql = "INSERT INTO 'totems'(mac, rfid) VALUES ('$mac', '$rfid')";
+$sql = "INSERT INTO 'totems'('mac', 'rfid') VALUES ('$mac', '$rfid')";
 
 $conn->query($sql);
