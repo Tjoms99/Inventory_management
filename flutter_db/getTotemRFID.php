0686526ffed101b93e5260422bce098a2b306a03
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-06-23 02:52:43 +0200

    Added updated server files

---
+++ flutter_db/getTotemRFID.php
@@ -0,0 +1,16 @@
+<?php
+include 'conn.php';
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$ip = '192.168.1.9';
+$res = array();
+
+
+$sql = "SELECT *  FROM `totems` WHERE `ip` LIKE '$ip'";
+
+$res = $conn->query($sql);
+
+echo json_encode($res->fetch_assoc()['rfid']);
+
+$sql = "UPDATE `totems` SET `rfid` = '' WHERE `totems`.`ip` = '$ip'";
+$conn->query($sql);
---
+++ flutter_db/updateAccountRegisteredCustomerID.php
@@ -0,0 +1,10 @@
+<?php
+include 'conn.php';
+
+$id = $_POST['id'];
+$registered_customer_id = $_POST['registered_customer_id'];
+
+$id = intval($id);
+
+$sql = "UPDATE `accounts` SET `registered_customer_id` = '$registered_customer_id' WHERE `accounts`.`id` = $id";
+$conn->query($sql);
