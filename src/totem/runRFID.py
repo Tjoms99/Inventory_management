b9cc46038646189bb9f66ff75a12fe4a5c33e76a
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-06-24 01:38:10 +0200

    Added updated dart docs + changed ip in python script

--- src/totem/runRFID.py
+++ src/totem/runRFID.py
@@ -6,33 +6,29 @@ from getmac import get_mac_address		# library for getting device mac address
 from mfrc522 import SimpleMFRC522		# RFID module library
 import RPi.GPIO as GPIO					# Raspberry pi GPIO library
 
-api_endpoint = "http://192.168.1.201/dashboard/flutter_db/addTotem.php"
+api_endpoint = "http://192.168.137.15/dashboard/flutter_db/addTotem.php"
 mac = get_mac_address()
 
 reader = SimpleMFRC522()
 
-# Function waits for an RFID tag and makes a request to the web-server, and repeats. 
+# Function waits for an RFID tag and makes a request to the web-server, and repeats.
+
+
 def main():
-	while True:
-		rfid, text = reader.read()
-		rfid = hex(rfid)
-		data = {'rfid':rfid}
-		r = requests.post(url = api_endpoint, data = data)
-		time.sleep(3)
+    while True:
+        rfid, text = reader.read()
+        rfid = hex(rfid)
+        data = {'rfid': rfid}
+        r = requests.post(url=api_endpoint, data=data)
+        time.sleep(3)
+
 
 def terminate():
-	GPIO.cleanup()
+    GPIO.cleanup()
+
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        terminate()	
-	
-
-
-
-
-
-
-
+        terminate()
