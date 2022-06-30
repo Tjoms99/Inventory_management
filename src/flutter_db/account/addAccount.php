1ed865bb05890569b7a62ba2cc266f4b4c2e023c
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-06-30 18:25:54 +0200

    Added error checking for account name and rfid

--- src/flutter_db/account/addAccount.php
+++ src/flutter_db/account/addAccount.php
@@ -11,6 +11,27 @@ $registered_customer_id = $_POST['registered_customer_id'];
 
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO `accounts`(`account_name`, `account_role`, `password`, `rfid`, `customer_id`, `registered_customer_id`) VALUES ('$account_name','$account_role','$hashed_password','$rfid','$customer_id', '$registered_customer_id')";
+//Chech if account exists
+$sql = "SELECT * FROM `accounts` WHERE `account_name` LIKE '$account_name'";
+$res = $conn->query($sql);
+if (mysqli_num_rows($res) != 0) {
+    return;
+}
 
+//Check if rfid in accounts exists
+$sql = "SELECT * FROM `accounts` WHERE `rfid` LIKE '$rfid'";
+$res = $conn->query($sql);
+if (mysqli_num_rows($res) != 0) {
+    return;
+}
+
+//Check if rfid in items exists
+$sql = "SELECT * FROM `items` WHERE `rfid` LIKE '$rfid'";
+$res = $conn->query($sql);
+if (mysqli_num_rows($res) != 0) {
+    return;
+}
+
+//Insert account
+$sql = "INSERT INTO `accounts`(`account_name`, `account_role`, `password`, `rfid`, `customer_id`, `registered_customer_id`) VALUES ('$account_name','$account_role','$hashed_password','$rfid','$customer_id', '$registered_customer_id')";
 $conn->query($sql);
