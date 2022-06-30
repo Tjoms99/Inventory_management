c8046f9b1e21b59df9cdfd1afd31499fb3795c81
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-06-30 18:28:38 +0200

    Added error checking for items rfid

--- src/flutter_db/item/addItem.php
+++ src/flutter_db/item/addItem.php
@@ -10,6 +10,20 @@ $location = $_POST['location'];
 $registered_customer_id = $_POST['registered_customer_id'];
 
 
-$sql = "INSERT INTO `items`(`name`, `status`, `rfid`, `description`, `location`, `registered_customer_id`) VALUES ('$name','$status','$rfid', '$description', '$location', '$registered_customer_id')";
+//Check if rfid in accounts exists
+$sql = "SELECT * FROM `accounts` WHERE `rfid` LIKE '$rfid'";
+$res = $conn->query($sql);
+if (mysqli_num_rows($res) != 0) {
+    return;
+}
 
+//Check if rfid in items exists
+$sql = "SELECT * FROM `items` WHERE `rfid` LIKE '$rfid'";
+$res = $conn->query($sql);
+if (mysqli_num_rows($res) != 0) {
+    return;
+}
+
+
+$sql = "INSERT INTO `items`(`name`, `status`, `rfid`, `description`, `location`, `registered_customer_id`) VALUES ('$name','$status','$rfid', '$description', '$location', '$registered_customer_id')";
 $conn->query($sql);
