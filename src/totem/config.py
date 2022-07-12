dd0808d51cff1e13e36cd6679845bbc58c694881
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-07-12 15:45:18 +0200

    Split config script and rfid script

---
+++ src/totem/config.py
@@ -0,0 +1,25 @@
+import string
+import random
+import RPi.GPIO as GPIO
+
+
+def config():
+    characters = string.ascii_letters + string.digits
+    totem_id = ''.join(random.choice(characters) for i in range(8))
+    try:
+        f = open('config.txt', 'r')
+        totem_id = f.readline()
+        f.close()
+    except:
+        f = open('config.txt', 'w')
+        f.write(id)
+        f.close()
+    finally:
+        return totem_id
+
+
+if __name__ == '__main__':
+    try:
+        config()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
--- src/totem/runRFID.py
+++ src/totem/runRFID.py
@@ -1,27 +1,11 @@
-import string
-import random
 import time
 import requests
 from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
+import config
 
 api_endpoint = "http://192.168.43.90/dashboard/flutter_db/totem/addTotem.php"
 reader = SimpleMFRC522()
-
-
-def config():
-    characters = string.ascii_letters + string.digits
-    totem_id = ''.join(random.choice(characters) for i in range(8))
-    try:
-        f = open('config.txt', 'r')
-        totem_id = f.readline()
-        f.close()
-    except:
-        f = open('config.txt', 'w')
-        f.write(id)
-        f.close()
-    finally:
-        return totem_id
 
 
 def main():
