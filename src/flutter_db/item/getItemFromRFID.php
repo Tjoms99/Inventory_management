a26ba0a5d97babe5d86334ae0f54cf8efad4c3ba
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-07-14 15:28:46 +0200

    Fixed bugs in get items php

--- src/flutter_db/item/getItemFromRFID.php
+++ src/flutter_db/item/getItemFromRFID.php
@@ -2,11 +2,13 @@
 include '../conn.php';
 
 $rfid = $_POST['rfid'];
+$rfid = "4CE340FE";
+$sql = "SELECT * FROM ITEMS WHERE rfid LIKE '$rfid'";
+$res = $conn->query($sql);
 
-$sql = $conn->query("SELECT * FROM ITEMS WHERE 'rfid' LIKE '$rfid'");
 
 if (mysqli_num_rows($res) != 0) {
-    $res = mysqli_fetch_assoc($conn->query($sql));
+    $res = mysqli_fetch_assoc($res);
     echo json_encode($res);
     return;
 }
--- src/flutter_db/item/getItems.php
+++ src/flutter_db/item/getItems.php
@@ -3,18 +3,20 @@ include '../conn.php';
 
 $account_role = $_POST['account_role'];
 $customer_id = $_POST['customer_id'];
-
-if ($account_role = 'admin'){
-    $sql = $conn->query("SELECT * FROM ITEMS");
-}
-else{
-    $sql = $conn->query("SELECT * FROM ITEMS WHERE 'customer_id' LIKE '$customer_id'");
-}
-
 $res = array();
 
-while ($row = $sql->fetch_assoc()) {
-    $res[] = $row;
-}
+$pos = strpos($customer_id, '1');
 
+$sql = "SELECT * FROM ITEMS";
+$sql = $conn->query($sql);
+
+
+//Add items depending on role and registered customer id.
+while ($row = $sql->fetch_assoc()) {
+    if ($account_role == "admin") {
+        $res[] = $row;
+    } elseif (substr($row['registered_customer_id'], $pos, 1) == "1" && $pos !== false) {
+        $res[] = $row;
+    }
+}
 echo json_encode($res);
