a876743e858f5bd022ed13a5d013c72daa01bb77
anh60 <andreas.holleland@outlook.com>
2022-07-15 17:29:07 +0200

    Changed python script to support rfid as totem_id

--- src/totem/runRFID.py
+++ src/totem/runRFID.py
@@ -2,17 +2,37 @@ import time
 import requests
 from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
-from config import *
 
-api_endpoint = "http://192.168.43.90/dashboard/flutter_db/totem/addTotem.php"
+api_endpoint = "http://192.168.1.201/dashboard/flutter_db/totem/addTotem.php"
 reader = SimpleMFRC522()
 
 
+def config():
+    try:
+        f = open('config.txt', 'r')
+        totem_id = f.readline()
+        f.close()
+    except:
+        f = open('config.txt', 'w')
+        totem_id, text = reader.read()
+        totem_id = hex(totem_id)
+        totem_id = totem_id[2:-2]
+        f.write(totem_id)
+        f.close()
+        
+        data = {'totem_id': totem_id, 'rfid': ''}
+        r = requests.post(url=api_endpoint, data=data)
+        print(data)
+    finally:
+        return totem_id
+
 def main():
     totem_id = config()
+    print(totem_id)
     while True:
         rfid, text = reader.read()
         rfid = hex(rfid)
+        rfid = rfid[2:-2]
         data = {'totem_id': totem_id, 'rfid': rfid}
         r = requests.post(url=api_endpoint, data=data)
         time.sleep(2)
