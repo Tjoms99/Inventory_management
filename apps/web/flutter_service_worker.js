79595dc5040fb3723a956e702e0553101a5e7568
Marcus Alexander Tjomsaas s299571 <s299571@studenti.polito.it>
2022-07-17 16:05:34 +0200

    Builded for windows and web

--- apps/web/flutter_service_worker.js
+++ apps/web/flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9e798ba355547a309dd50da6a9b5b801",
-"/": "9e798ba355547a309dd50da6a9b5b801",
-"main.dart.js": "805aece8c311aa2c1b5e659dab607bad",
+"index.html": "bb0ea3bf2baff0b6481d3bb600dc12f9",
+"/": "bb0ea3bf2baff0b6481d3bb600dc12f9",
+"main.dart.js": "c0ba175960a0ea2646d8f93c8c2ac405",
 "manifest.json": "11f01a83af7e6402fe350833686b1005",
 "version.json": "6e502247b813c9e8e96d7f7c5f7b90c2"
 };
